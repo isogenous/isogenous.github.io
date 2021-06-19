19da47fe4ada6a02eb83f825532de7c27efe69ce
oiuoyt <oiuoyt@gmail.com>
2021-06-19 15:31:08 -0400

    fixup footnote gen

--- js/footnotes.js
+++ js/footnotes.js
@@ -4,14 +4,14 @@ function convert_text_to_footnotes() {
   var footnote_num =0;
 
   var link_to_footnote_template ='<a name="back0" rel="nofollow" id="back0"></a><a href="#note0" rel="nofollow"><sup>0</sup></a>'
-  var link_back_template ='<li><sup><a href="#back0" rel="nofollow">[^]</a></sup><a name="note0" rel="nofollow" id="note0"></a>FOOTNOTE</li>'
+  var link_back_template ='<li><sup><a href="#back0" rel="nofollow">[^]</a></sup> <a name="note0" rel="nofollow" id="note0"></a>FOOTNOTE</li>'
 
   var all_footnotes_html='<ol>'
 
   var html_with_footnotes = original_html.replace(/[\[\(]FOOTNOTE[\:\s](.*)[\]\)]/g, (match, $1) => {
     footnote_num++
 
-    var link_back_html = link_back_template.replaceAll('0', footnote_num).replaceAll('FOOTNOTE', $1).trim()
+    var link_back_html = link_back_template.replaceAll('0', footnote_num).replaceAll('FOOTNOTE', $1.trim())
     all_footnotes_html = all_footnotes_html.concat(link_back_html)
 
     return link_to_footnote_template.replaceAll('0', footnote_num)
