3c055fd5b642fba27dc26c6ba5535153f5e1d6aa
isogenous <82745014+isogenous@users.noreply.github.com>
2021-06-21 15:30:59 -0700

    uuid for footnotes

--- js/footnotes.js
+++ js/footnotes.js
@@ -1,9 +1,18 @@
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+
 function convert_text_to_footnotes() {
   var original_html = document.getElementById("original_html").value.replaceAll('</p>', '</p>\r\n')
 
   var footnote_num =0;
 
-  var link_to_footnote_template ='<a name="back0" rel="nofollow" id="back0"></a><a href="#note0" rel="nofollow"><sup>0</sup></a>'
+
+  var link_to_footnote_template ='<a name="back0" rel="nofollow" id="back0"></a><a href="#note0" rel="nofollow"><sup>number</sup></a>'
   var link_back_template ='<li><sup><a href="#back0" rel="nofollow">[^]</a></sup> <a name="note0" rel="nofollow" id="note0"></a>FOOTNOTE</li>\r\n'
 
   var all_footnotes_html='<ol>'
@@ -11,10 +20,10 @@ function convert_text_to_footnotes() {
   var html_with_footnotes = original_html.replace(/[\[\(]FOOTNOTE[\:\s](.*)[\]\)]/g, (match, $1) => {
     footnote_num++
 
-    var link_back_html = link_back_template.replaceAll('0', footnote_num).replaceAll('FOOTNOTE', $1.trim())
+    var link_back_html = link_back_template.replaceAll('back0', id1).replaceAll('note0', id2).replaceAll('FOOTNOTE', $1.trim())
     all_footnotes_html = all_footnotes_html.concat(link_back_html)
 
-    return link_to_footnote_template.replaceAll('0', footnote_num)
+    return link_to_footnote_template.replaceAll('number', footnote_num).replaceAll('back0', id1).replaceAll('note0', id2)
   });
 
 all_footnotes_html = all_footnotes_html.concat("</ol>")
